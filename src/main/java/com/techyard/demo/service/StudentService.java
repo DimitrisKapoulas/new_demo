a96af9e6502e4beb4e80ebe74c727c43f59fc2a4
DimitrisKapoulas <kapoulasd@gmail.com>
2019-09-04 23:21:26 +0300

    API Layer(HTTP POST)

--- src/main/java/com/techyard/demo/service/StudentService.java
+++ src/main/java/com/techyard/demo/service/StudentService.java
@@ -21,6 +21,7 @@ public class StudentService {
 
     public int persistNewStudent(UUID studentId, Student student){
         UUID studentUid = studentId == null ? UUID.randomUUID() : studentId;
+        student.setId(studentId);
         return studentDao.insertNewStudent(studentUid, student);
     }
 
