7bdba4fefc13d322908e7d279218344f37d8be6b
DimitrisKapoulas <kapoulasd@gmail.com>
2019-09-05 00:18:33 +0300

    PUT and DELETE added

--- src/main/java/com/techyard/demo/resource/StudentResource.java
+++ src/main/java/com/techyard/demo/resource/StudentResource.java
@@ -4,10 +4,7 @@ import com.techyard.demo.model.Student;
 import com.techyard.demo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
@@ -33,11 +30,38 @@ public class StudentResource {
     }
 
     @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            path = "{studentId}"
+    )
+    public Student getStudentById(@PathVariable("studentId") UUID studentId) {
+        return studentService.getStudentById(studentId);
+    }
+
+    @RequestMapping(
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_JSON_VALUE
     )
     public void insertNewStudent(@RequestBody Student student) {
         studentService.persistNewStudent(UUID.randomUUID(), student);
     }
+
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            path = "{studentId}"
+    )
+    public void updateStudent(@PathVariable("studentId") UUID studentId, @RequestBody Student student) {
+        studentService.updateStudentById(studentId, student);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.DELETE,
+            path = "{studentId}"
+    )
+    public void deleteStudent(@PathVariable("studentId") UUID studentId) {
+        studentService.deleteStudentById(studentId);
+    }
+
 
 }
